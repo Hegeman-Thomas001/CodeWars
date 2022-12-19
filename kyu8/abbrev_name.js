5e5e576a64a5342738b8ed185198c8bce990392c
Thomas Hegeman <hegemant001@gmail.com>
2022-12-19 12:15:32 -0500

    added abbrev_name

---
+++ kyu8/abbrev_name.js
@@ -0,0 +1,15 @@
+function abbrevName(name) {
+  // code away
+  const nameArr = name.split(" ");
+  const retArr = [];
+
+  for (const name of nameArr) {
+    retArr.push(name[0].toUpperCase());
+  }
+
+  return retArr.join(".");
+}
+
+console.log(abbrevName("Sam Harris"));
+console.log(abbrevName("Patrick Feenan"));
+console.log(abbrevName("Evan Cole"));
