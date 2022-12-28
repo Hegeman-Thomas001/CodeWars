a8d76be13aeb63918384ece99b00e08b1316cb3f
Thomas Hegeman <hegemant001@gmail.com>
2022-12-28 09:21:59 -0500

    added sum_of_minimums

---
+++ kyu7/sum_of_minimums.js
@@ -0,0 +1,18 @@
+function sumOfMinimums(arr) {
+  // your code here
+  const minValsArr = [];
+  for (let arrVals of arr) {
+    const minVal = Math.min(...arrVals);
+    minValsArr.push(minVal);
+  }
+
+  return minValsArr.reduce((acc, curr) => acc + curr, 0);
+}
+
+console.log(
+  sumOfMinimums([
+    [7, 9, 8, 6, 2],
+    [6, 3, 5, 4, 3],
+    [5, 8, 7, 4, 5],
+  ])
+);
