0e8a64bea5ef73319ed538def395d957f6e61ad3
Thomas Hegeman <hegemant001@gmail.com>
2023-01-01 08:27:25 -0500

    added century

--- kyu8/century.js
+++ kyu8/century.js
@@ -8,6 +8,7 @@ function century(year) {
 
   // codewars style
   // return year % 100 === 0 ? year / 100 : (year - (year % 100)) / 100 + 1;
+  // return Math.ceil(year / 100);
 }
 
 console.log(century(1705));
