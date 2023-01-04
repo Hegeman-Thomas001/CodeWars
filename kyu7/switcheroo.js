d54ad068c017884dc0b234a0126927bec95557a8
Thomas Hegeman <hegemant001@gmail.com>
2023-01-04 12:23:07 -0500

    added switcheroo

---
+++ kyu7/switcheroo.js
@@ -0,0 +1,21 @@
+function switcheroo(x) {
+  const letterMap = new Map([
+    ["a", "b"],
+    ["b", "a"],
+  ]);
+  const arrToStr = [];
+
+  for (let chara of x) {
+    const charaOpp = letterMap.get(chara);
+
+    if (charaOpp) {
+      arrToStr.push(charaOpp);
+    } else {
+      arrToStr.push(chara);
+    }
+  }
+
+  return arrToStr.join("");
+}
+
+console.log(switcheroo("aaabcccbaaa"));
