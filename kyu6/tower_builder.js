670f95bf2647b41ee6e092f9f9b3c7fe4609a8f7
Thomas Hegeman <hegemant001@gmail.com>
2023-01-06 19:37:52 -0500

    added tower_builder

--- kyu6/tower_builder.js
+++ kyu6/tower_builder.js
@@ -1,17 +1,12 @@
 function towerBuilder(nFloors) {
-  // build here
-  // 2i + 1
   const arrRet = [];
-  const strStar = "*";
 
   for (let i = 0; i < nFloors; i++) {
     const repeatVal = 2 * i + 1;
     const space = nFloors - 1 - i;
-    const padStart = space + repeatVal;
-    const padEnd = padStart + space;
-    const strMid = `${strStar.repeat(repeatVal)}`;
-    const strPadStart = `${strMid.padStart(padStart, " ")}`;
-    const fullStr = `${strPadStart.padEnd(padEnd, " ")}`;
+    const fullStr = `${" ".repeat(space)}${"*".repeat(repeatVal)}${" ".repeat(
+      space
+    )}`;
     arrRet.push(fullStr);
   }
 
