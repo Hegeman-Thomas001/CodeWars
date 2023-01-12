9d96234169080ed38c06f4f3a2e131530a5d1b7f
Thomas Hegeman <hegemant001@gmail.com>
2023-01-12 08:05:07 -0500

    added wide_mouth

---
+++ kyu8/wide_mouth.js
@@ -0,0 +1,7 @@
+function mouthSize(animal) {
+  // code here
+  return animal.toLowerCase() === "alligator" ? "small" : "wide";
+}
+
+console.log(mouthSize("alligator"));
+console.log(mouthSize(""));
