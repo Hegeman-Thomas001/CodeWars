32fdb6d3202cf55f351f01d2efe827a86387343e
Thomas Hegeman <hegemant001@gmail.com>
2023-01-16 11:46:00 -0500

    added mine_location

--- kyu6/mine_location.js
+++ kyu6/mine_location.js
@@ -1,28 +1,16 @@
 function mineLocation(field) {
   //TODO
-  const arrRet = [];
   const sentinel = field.length;
-  let isMine = false;
 
   for (let i = 0; i < sentinel; i++) {
-    //
     for (let j = 0; j < sentinel; j++) {
-      const val = field[i][j];
-
-      if (val === 1) {
-        isMine = true;
-        arrRet.push(i);
-        arrRet.push(j);
-        break;
+      if (field[i][j] === 1) {
+        return [i, j];
       }
-    }
-    //
-    if (isMine) {
-      break;
     }
   }
 
-  return arrRet;
+  return [];
 }
 
 console.log(
