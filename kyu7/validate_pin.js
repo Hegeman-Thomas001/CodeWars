e09af2ca6dd8692d65faff4ad6e12e2f4bc96aa4
Thomas Hegeman <hegemant001@gmail.com>
2023-01-21 10:59:27 -0500

    added validate_pin

---
+++ kyu7/validate_pin.js
@@ -0,0 +1,43 @@
+function validatePIN(pin) {
+  //return true or false
+  let isValidPIN = true;
+  const pinLength = pin.length;
+  console.log("0".charCodeAt(), "9".charCodeAt());
+
+  if (pinLength === 4 || pinLength === 6) {
+    for (const chara of pin) {
+      const charaCode = chara.charCodeAt();
+      if (charaCode < 48 || charaCode > 57) {
+        isValidPIN = false;
+        break;
+      }
+    }
+  } else {
+    isValidPIN = false;
+  }
+
+  return isValidPIN;
+  // regex or regular expression way, BORING
+  // return /^(\d{4}|\d{6})$/.test(pin);
+  //
+  // const pinLength = pin.length;
+
+  // if (pinLength === 4 || pinLength === 6) {
+  //   for (const chara of pin) {
+  //     const charaCode = chara.charCodeAt();
+  //     if (charaCode < 48 || charaCode > 57) {
+  //       return false;
+  //     }
+  //   }
+  //    return true;
+  // }
+
+  // return false;
+}
+
+console.log(validatePIN("1"));
+console.log(validatePIN("1234"));
+console.log(validatePIN("123456"));
+console.log(validatePIN("-123"));
+console.log(validatePIN("a123"));
+console.log(validatePIN(".123"));
