12aa6880555ff54da0978a8a0c3fd5070780351d
Thomas Hegeman <hegemant001@gmail.com>
2023-01-27 11:23:43 -0500

    b

--- kyu8/str_count.js
+++ kyu8/str_count.js
@@ -1,7 +1,16 @@
 function strCount(str, letter) {
   //code here
+  let count = 0;
+
+  for (const chara of str) {
+    if (chara === letter) {
+      ++count;
+    }
+  }
+
+  return count;
 }
 
-console.log(strCount());
-console.log(strCount());
-console.log(strCount());
+console.log(strCount("Hello", "o"));
+console.log(strCount("Hello", "l"));
+console.log(strCount("", "z"));
