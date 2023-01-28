71932b2ed90cf69d15a561839a2f5584bdfeaac3
Thomas Hegeman <hegemant001@gmail.com>
2023-01-28 10:11:43 -0500

    added get_divisor_count

---
+++ kyu7/get_divisor_count.js
@@ -0,0 +1,22 @@
+function getDivisorsCnt(n) {
+  // todo
+  if (n === 1) {
+    return 1;
+  }
+
+  let count = 2;
+  const sentinel = Math.ceil(n / 2);
+
+  for (let i = 2; i < sentinel; i++) {
+    if (n % i === 0) {
+      ++count;
+    }
+  }
+
+  return count;
+}
+
+console.log(getDivisorsCnt(1));
+console.log(getDivisorsCnt(10));
+console.log(getDivisorsCnt(11));
+console.log(getDivisorsCnt(54));
