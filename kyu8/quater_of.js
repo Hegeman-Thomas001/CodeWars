9d20073a05c8e13bf8b89234ed73e60b3897dcf2
Thomas Hegeman <hegemant001@gmail.com>
2023-01-28 09:45:37 -0500

    added quater_of

---
+++ kyu8/quater_of.js
@@ -0,0 +1,13 @@
+const quarterOf = (month) => {
+  // Your code here
+  return Math.ceil(month / 3);
+};
+
+console.log(quarterOf(1));
+console.log(quarterOf(3));
+console.log(quarterOf(5));
+console.log(quarterOf(6));
+console.log(quarterOf(8));
+console.log(quarterOf(9));
+console.log(quarterOf(11));
+console.log(quarterOf(12));
