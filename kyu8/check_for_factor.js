78b49e3053385c3f13559c4319a46b72c5dd8c6f
Thomas Hegeman <hegemant001@gmail.com>
2023-01-30 11:08:09 -0500

    check for factor change

---
+++ kyu8/check_for_factor.js
@@ -0,0 +1,9 @@
+function checkForFactor(base, factor) {
+  // code here
+  return base % factor === 0;
+}
+
+console.log(checkForFactor(10, 2));
+console.log(checkForFactor(10, 3));
+console.log(checkForFactor(63, 7));
+console.log(checkForFactor(2450, 5));
